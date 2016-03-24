1c8de6806a283ac357e71868c4de11f84097da79
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-03-24 21:37:26 +0900

    Add Dual Site Langmuir isotherm

--- iast_organ/isotherm_organ/isotherms/DSL_isotherm.hpp
+++ iast_organ/isotherm_organ/isotherms/DSL_isotherm.hpp
@@ -5,10 +5,13 @@
 class Langmuir_isotherm : public isotherm_base
     {
 public:
-    Langmuir_isotherm(real_t in_q_sat, real_t in_K)
+    Langmuir_isotherm(real_t in_q_sat1, real_t in_q_sat2
+                      real_t in_K1,     real_t in_K2)
         :
-        q_sat {in_q_sat},
-        K  {in_K}
+        q_sat1 {in_q_sat1},
+        q_sat2 {in_q_sat2},
+        K1  {in_K1}
+        K2  {in_K2}
         {
         // do nothing.    
         }
@@ -17,18 +20,18 @@ public:
     loading(real_t T, real_t P) 
         override
         {
-        return q_sat * K * P / (1.0 + K * P);
+        return q_sat1 * K1 * P / (1.0 + K1 * P) + q_sat2 * K2 * P / (1.0 + K2 * P);
         }
         
     real_t 
     spreading_pressure(real_t T, real_t P) 
         override
         {
-        return q_sat * std::log(1.0 + K * P);
+        return q_sat1 * std::log(1.0 + K1 * P) + q_sat2 * std::log(1.0 + K2 * P);
         }       
-        
 private:
-    double q_sat;
-    double K;
-    
+    double q_sat1;
+    double q_sat2;
+    double K1;
+    double K2;
     };
