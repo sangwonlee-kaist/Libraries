9e0d78b325930a1e001161f4f25cdf00f321e22d
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-05-11 15:44:40 +0900

    Commit all simplex changes.

--- simplex/simplex_impl.hpp
+++ simplex/simplex_impl.hpp
@@ -9,19 +9,17 @@
     #define DEBUG2(x, y)
 #endif
 
-simplex::simplex(size_t dimension)
-    :
-    m_dimension (dimension)
+simplex::simplex(size_t dimension) : m_dimension (dimension)
     {
     }
 
 double 
-simplex::optimize(double *point, double(*func)(double *, int), double tol)
+simplex::optimize(double* point, double(*func)(double*, int), double tol)
     {
     int i;
     int dim = m_dimension;
 
-    double * del = alloc_vector(dim);
+    double* del = alloc_vector(dim);
     // 10 % of initial position.
     for (i = 0; i < dim; i++)
         {
@@ -42,58 +40,30 @@ simplex::optimize(double *point, double(*func)(double *, int), double tol)
     return fmin;
     }
 
-double 
-simplex::global_optimize(double *point, double(*func)(double *, int), double tol)
-    {
-    int i;
-    int dim = m_dimension;
-
-    double * del = alloc_vector(dim);
-//  10 % of initial position.
-    for (i = 0; i < dim; i++)
-        {
-        point[i] = urand();
-        del[i] = 0.1 * point[i];    
-        DEBUG(del[i])
-        }
-
-    for (i = 0; i < dim; i++)
-        {
-        del[i] = (del[i] < 1.e-5 ? 1.e-5 : del[i]);
-        DEBUG(del[i])
-        }
-   
-    double fmin = amoeba2(point, dim, del, func, tol);
-
-    free_vector(del, dim);
-
-    return fmin;
-    }
-
-double * 
+double* 
 simplex::alloc_vector(int cols)
     {
-    return (double *)std::malloc(sizeof(double) * cols);
+    return (double* )std::malloc(sizeof(double) * cols);
     }
 
 void 
-simplex::free_vector(double * vector, int cols)
+simplex::free_vector(double* vector, int cols)
     {
     std::free(vector);
     }
 
-double ** 
+double** 
 simplex::alloc_matrix(int rows, int cols)
     {
     int i;
-    double ** matrix = (double **)std::malloc(sizeof(double *) * rows);
+    double** matrix = (double**)std::malloc(sizeof(double* ) * rows);
     for (i = 0; i < rows; i++)
         matrix[i] = alloc_vector(cols);
     return matrix;
     }
 
 void 
-simplex::free_matrix(double ** matrix, int rows, int cols)
+simplex::free_matrix(double** matrix, int rows, int cols)
     {
     int i;
     for (i = 0; i < rows; i++)
@@ -101,11 +71,11 @@ simplex::free_matrix(double ** matrix, int rows, int cols)
     std::free(matrix);
     }
 
-double ** 
-simplex::make_simplex(double * point, int dim, double * del)
+double** 
+simplex::make_simplex(double* point, int dim, double* del)
     {
     int i, j;
-    double ** simplex = alloc_matrix(dim + 1, dim);
+    double** simplex = alloc_matrix(dim + 1, dim);
     for (i = 0; i < dim + 1; i++)
         for (j = 0; j < dim; j++)
             simplex[i][j] = point[j];
@@ -115,8 +85,8 @@ simplex::make_simplex(double * point, int dim, double * del)
     }   
 
 void 
-simplex::evaluate_simplex(double ** simplex, int dim, double * fx, 
-    double(*func)(double *, int))
+simplex::evaluate_simplex(double** simplex, int dim, double* fx, 
+    double(*func)(double* , int))
     {
     // simplex::evaluate_simplex(): 
     // evaluate function value for each simpelx point.
@@ -126,8 +96,8 @@ simplex::evaluate_simplex(double ** simplex, int dim, double * fx,
     }
 
 void 
-simplex::simplex_extremes(double *fx, int dim, int *ihi, int *ilo,
-    int *inhi)
+simplex::simplex_extremes(double* fx, int dim, int* ihi, int* ilo,
+    int* inhi)
     {
     // simlex::simplex_extreams(): 
     // find order of between function values of simplex points.
@@ -156,8 +126,8 @@ simplex::simplex_extremes(double *fx, int dim, int *ihi, int *ilo,
     }
 
 void 
-simplex::simplex_bearings(double ** simplex, int dim,
-    double * midpoint, double * line, int ihi)
+simplex::simplex_bearings(double** simplex, int dim,
+    double* midpoint, double* line, int ihi)
     {
     // simplex::simplex_bearings(): 
     // make two vectors used in simplex trial moves.
@@ -178,14 +148,14 @@ simplex::simplex_bearings(double ** simplex, int dim,
     }
 
 int 
-simplex::update_simplex(double * point, int dim, double * fmax,
-    double * midpoint, double * line, double scale,
-    double(*func)(double *, int))
+simplex::update_simplex(double* point, int dim, double* fmax,
+    double* midpoint, double* line, double scale,
+    double(*func)(double* , int))
     {
     // simplex::update_simplex():
     // make trial points and change to maximum point if
     // new point is lower than previous maximum point
-    int i, update = 0; double * next = alloc_vector(dim), fx;
+    int i, update = 0; double* next = alloc_vector(dim), fx;
     for (i = 0; i < dim; i++)
         next[i] = midpoint[i] + scale * line[i];
     fx = (*func)(next, dim);
@@ -198,32 +168,10 @@ simplex::update_simplex(double * point, int dim, double * fmax,
     return update;
     }
 
-int 
-simplex::update_simplex2(double * point, int dim, double * fmax,
-    double * midpoint, double * line, double scale, double temperature,
-    double(*func)(double *, int))
-    {
-    // simplex::update_simplex2():
-    // make trial points and change to maximum point if
-    // boltzmann distribution is accepted randomly.
-    int i, update = 0; double * next = alloc_vector(dim), fx, df;
-    for (i = 0; i < dim; i++)
-        next[i] = midpoint[i] + scale * line[i];
-    fx = (*func)(next, dim);
-    df = fx - *fmax;
-    if (urand() < std::exp(-df / temperature))
-        {
-        for (i = 0; i < dim; i++) point[i] = next[i];
-        *fmax = fx; update = 1;
-        }
-    free_vector(next, dim);
-    return update;
-    }
-
 void 
-simplex::contract_simplex(double ** simplex, int dim,
-    double * fx, int ilo,
-    double(*func)(double *, int))
+simplex::contract_simplex(double** simplex, int dim,
+    double* fx, int ilo,
+    double(*func)(double* , int))
     {
     int i, j;
     for (i = 0; i < dim + 1; i++)
@@ -245,16 +193,16 @@ simplex::check_tol(double fmax, double fmin, double ftol)
     }
 
 double 
-simplex::amoeba(double *point, int dim, double * del,
-    double(*func)(double *, int),
+simplex::amoeba(double* point, int dim, double* del,
+    double(*func)(double* , int),
     double tol)
     {
     int ihi, ilo, inhi, j;
     double fmin;
-    double * fx = alloc_vector(dim + 1);
-    double * midpoint = alloc_vector(dim);
-    double * line = alloc_vector(dim);
-    double ** simplex = make_simplex(point, dim, del);
+    double* fx = alloc_vector(dim + 1);
+    double* midpoint = alloc_vector(dim);
+    double* line = alloc_vector(dim);
+    double** simplex = make_simplex(point, dim, del);
     evaluate_simplex(simplex, dim, fx, func);
 
     size_t max_iter = 10000;
@@ -281,151 +229,6 @@ simplex::amoeba(double *point, int dim, double * del,
 
     for (j = 0; j < dim; j++)
         point[j] = simplex[ilo][j];
-    fmin = fx[ilo];
-    free_vector(fx, dim);
-    free_vector(midpoint, dim);
-    free_vector(line, dim);
-    free_matrix(simplex, dim + 1, dim);
-    return fmin;
-    }
-
-double 
-simplex::urand()
-    {
-    static bool firstQ = true;
-#ifdef SIMPLEX_USE_CXX11
-    static std::mt19937 gen;
-    static std::uniform_real_distribution<double> dist {0.0, 1.0};
-
-    if (firstQ)            
-        {
-        auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-        gen.seed(seed);
-        firstQ = false;
-        }
-
-    return dist(gen);
-#else
-    if (firstQ)
-        {
-        std::srand(std::time(NULL));
-        firstQ = false;
-        }
-
-    return std::rand() / static_cast<double>(RAND_MAX);
-#endif
-    }
-
-double
-simplex::grand()
-    {
-    static bool firstQ = true;
-#ifdef SIMPLEX_USE_CXX11
-    static std::mt19937 gen;
-    static std::normal_distribution<double> gdist {1.0, 0.25};
-
-    if (firstQ)            
-        {
-        auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-        gen.seed(seed);
-        firstQ = false;
-        }
-
-    return gdist(gen);
-#else
-    double v1, v2, s;
-
-    do 
-        {
-        v1 = 2.0 * urand() - 1.0;
-        v2 = 2.0 * urand() - 1.0;
-        s  = v1 * v1 + v2 * v2;
-        } 
-    while (s >= 1.0 || s == 0);
-
-    s = sqrt(-2.0 * std::log(s) /s);
-
-    return 0.25 * v1 * s + 1.0;
-#endif
-   
-    }
-
-double 
-simplex::amoeba2(double *point, int dim, double * del,
-    double(*func)(double *, int),
-    double tol)
-    {
-    int ihi, ilo, inhi, j;
-    double fmin;
-    double * fx = alloc_vector(dim + 1);
-    double * midpoint = alloc_vector(dim);
-    double * line = alloc_vector(dim);
-    double ** simplex = make_simplex(point, dim, del);
-    evaluate_simplex(simplex, dim, fx, func);
-
-    size_t max_iter = 100 * m_dimension;
-    size_t iter = 0;
-    size_t max_temper_iter = 10000;
-    bool convergeQ = false;
-  
-    simplex_extremes(fx, dim, &ihi, &ilo, &inhi);
-
-    // T = 10 times of initial energy difference which is considerably large than 
-    // the largest delta E normmally encountered.
-    // T is decrease by 10 % for each step.
-    double T = 1.e1 * (fx[ihi] - fx[ilo]);
-    double prob;
-
-    for (size_t temper_iter {}; temper_iter < max_temper_iter; ++ temper_iter)
-        {
-        T *= 0.9;
-        DEBUG(T);
-        while (true)
-            {   
-            simplex_extremes(fx, dim, &ihi, &ilo, &inhi);
-            simplex_bearings(simplex, dim, midpoint, line, ihi);
-            if (check_tol(fx[ihi], fx[ilo], tol)) 
-                {
-                DEBUG(iter)
-                convergeQ = true;
-                break;
-                }
-            update_simplex2(simplex[ihi], dim, &fx[ihi], midpoint, line, -1.0 * grand(), T, func);
-            prob = std::exp(-(fx[ihi]-fx[ilo]) / T);
-            if (urand() < prob)
-                update_simplex2(simplex[ihi], dim, &fx[ihi], midpoint, line, -2.0 * grand(), T, func);
-            else if (fx[ihi] >= fx[inhi])
-                if (!update_simplex2(simplex[ihi], dim, &fx[ihi], midpoint, line, 0.5 * grand(), T, func))
-                    contract_simplex(simplex, dim, fx, ilo, func);
-
-            if (not std::isfinite(fx[ihi]))
-                {
-                throw std::runtime_error 
-                    ("error: objective function is not defined for given simplex.");
-                }
-
-            if (++iter >= max_iter)
-                {
-                DEBUG(iter)
-//                throw std::runtime_error ("error: simplex exceed maximum iteration.");
-                iter = 0;
-                break;
-                }
-//            DEBUG2(fx[ihi], fx[ilo])
-            }// end while
-            if (convergeQ)
-                {
-                DEBUG("amoeba2 is converged.")
-                break;
-                }
-        DEBUG(temper_iter)
-        DEBUG(fx[ihi])
-        
-        }// end temper iteration
-
-    for (j = 0; j < dim; j++)
-        point[j] = simplex[ilo][j];
-
     fmin = fx[ilo];
     free_vector(fx, dim);
     free_vector(midpoint, dim);
--- simplex/test/example1.cpp
+++ simplex/test/example1.cpp
@@ -8,7 +8,7 @@ int num_eval = 0;
 double func(double * p, int dim)
     {
     double x = p[0], y = p[1];
-    
+
     num_eval++;
 
     return -(y - x -2 * x * x - 2 * x * y - y * y);
@@ -22,9 +22,9 @@ main()
     double fmin = 0.0;
     // 2 = 2 dimensional problem.
     simplex optimizer2D (2);
-    
+
     fmin = optimizer2D.optimize(p, func, 1.e-15);
-    
+
     printf("min of f(x,y) = %12.6f\n", fmin);
     printf("at x = %12.6f\n", p[0]);
     printf("at y = %12.6f\n", p[1]);
