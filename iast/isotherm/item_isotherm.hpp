ca7b71710658ff3a1ec524175da655e0abeb4629
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-07-28 18:54:34 +0900

    Add ITEM functionality

--- iast/isotherm/item_isotherm.hpp
+++ iast/isotherm/item_isotherm.hpp
@@ -10,7 +10,7 @@ public:
 
     void setData(const std::string& filename);
     void setData(std::ifstream& ifs);
-    double operator () (double x);
+    double operator () (double x) const;
 private:
     std::vector<double> xs;
     std::vector<double> ys;
@@ -66,7 +66,7 @@ Interpolator::setData(std::ifstream& ifs)
     }
 
 double
-Interpolator::operator () (double x)
+Interpolator::operator () (double x) const
     {
     std::size_t i = std::lower_bound(xs.begin(), xs.end(), x) - xs.begin();
 
@@ -100,12 +100,24 @@ public:
 private:
     double                    mRefTemperature;
     const isotherm_base&      mRefIsotherm;
-    mutable IsothermContainer mTargetIsotherms;
-    Interpolator              mIsostericHeat;
+    // PressureList
+    // P | Uptake
+    // --+-------
+    // P0|  0.1
+    // P1|  0.2
+    // P3|  0.3
+    // ...
+    // Here, Pi is PressureList!
+    // Which is obtained using reference isotherm.
+    // We can access to element using
+    mutable std::vector<double> mPressureList;
+    mutable IsothermContainer   mTargetIsotherms;
+    Interpolator                mIsostericHeat;
 
-    void checkAndExpand(const std::string& Tstr, double P) const;
+    void checkAndExpand(double T, double P) const;
 public:
     double inverseIsotherm(double loading) const;
+    void   printPressureList() const;
     };
 
 ItemIsotherm::ItemIsotherm(const isotherm_base& isothermBase,
@@ -122,99 +134,248 @@ ItemIsotherm::ItemIsotherm(const isotherm_base& isothermBase,
 ItemIsotherm::RealType
 ItemIsotherm::loading(RealType T, RealType P) const
     {
+    auto& Tref = mRefTemperature;
+    auto& iso  = mRefIsotherm;
     std::string Tstr = std::to_string(T);
 
-    if (Tstr == std::to_string(mRefTemperature))
-       return mRefIsotherm.loading(T, P);
+    if (Tstr == std::to_string(Tref))
+       return iso.loading(T, P);
 
-    this->checkAndExpand(Tstr, P);
+    this->checkAndExpand(T, P);
 
-    return 0.0;
+    return mTargetIsotherms[Tstr].loading(T, P);
     }
 
 ItemIsotherm::RealType
 ItemIsotherm::spreading_pressure(RealType T, RealType P) const
     {
+    auto& Tref = mRefTemperature;
+    auto& iso  = mRefIsotherm;
     std::string Tstr = std::to_string(T);
 
-    if (Tstr == std::to_string(mRefTemperature))
-        return mRefIsotherm.spreading_pressure(T, P);
+    if (Tstr == std::to_string(Tref))
+        return iso.spreading_pressure(T, P);
 
-    this->checkAndExpand(Tstr, P);
+    this->checkAndExpand(T, P);
 
-    return 0.0;
+    return mTargetIsotherms[Tstr].spreading_pressure(T, P);
     }
 
 void
-ItemIsotherm::checkAndExpand(const std::string& Tstr, double P) const
+ItemIsotherm::checkAndExpand(double T, double P) const
     {
+    auto& Tref = mRefTemperature;
+    std::string Tstr = std::to_string(T);
     // Check temperature data exist.
     if (mTargetIsotherms.count(Tstr) == 0)
-        {
         mTargetIsotherms[Tstr] = interpolation_isotherm {};
-        }
 
     interpolation_isotherm& iso = mTargetIsotherms[Tstr];
+    auto& PList = mPressureList;
+    auto& Q     = mIsostericHeat;
+
+    double tiny  = 1.e-10;
+    double R     = 0.008314469;
+    double dbeta = (1.0 / T - 1.0 / Tref) / R;
 
     if (iso.empty())
         {
         // push first data.
+        if (PList.empty())
+            PList.push_back(this->inverseIsotherm(0.1));
 
+        double Ptar = PList[0] * std::exp(-dbeta * Q(0.1));
+        //double Ptar = (this->inverseIsotherm(0.1)) * std::exp(-dbeta * Q(0.1));
+        iso.push_back(Ptar, 0.1);
         }
 
     while (P > iso.getMaxPressure())
         {
         // Expand data.
-        }
+        double n = iso.getMaxLoading() + 0.1;
+        int index = static_cast<int>(n / 0.1 - tiny);
 
+        if (PList.size() <= index)
+            {
+            for (double nn = (PList.size() + 1) * 0.1; nn <= n + tiny; nn += 0.1)
+                PList.push_back(this->inverseIsotherm(nn));
+            }
+
+        double Ptar = PList[index] * std::exp(-dbeta * Q(n));
+        //double Ptar = (this->inverseIsotherm(n)) * std::exp(-dbeta * Q(n));
+        iso.push_back(Ptar, n);
+        }
     }
 
-// It would be nice if we implement Invertible decolator.
 double
 ItemIsotherm::inverseIsotherm(double loading) const
     {
-    // Secant Method.
-    double oldP = 1.0;
-    double newP = 0.0;
-    double oldN = 0.0;
-    double newN = 0.0;
+    // Simple Bisection Algorithm
+
+    double xLow    = 0.0;
+    double xHigh   = 0.0;
+    double xCenter = 0.0;
+
+    double fLow    = 0.0;
+    double fHigh   = 0.0;
+    double fCenter = 0.0;
+
     int iter = 0;
     int maxIter = 100;
 
-    double loadingGuess = mRefIsotherm.loading(mRefTemperature, oldP);
-    while (loadingGuess < loading)
+    auto& iso = mRefIsotherm;
+    auto& T   = mRefTemperature;
+
+    xLow = 0.0;
+    fLow = -loading;
+
+    xHigh = 1.0;
+    fHigh = iso.loading(T, xHigh) - loading;
+
+    while (fHigh < 0.0)
         {
-        oldP *= 1.5;
-        loadingGuess = mRefIsotherm.loading(mRefTemperature, oldP);
+        xHigh *= 1.5;
+        fHigh  = iso.loading(T, xHigh) - loading;
+
+        if (xHigh > 10000.0)
+            {
+            throw std::runtime_error
+                {"ItemIsotherm::inverseIsotherm(): Given uptake beyonds saturation loading."};
+            }
         }
 
-    oldN = 0.0;
-    newN = loadingGuess;
-
-    // oldP >> newP >> nextP ...
+    maxIter = static_cast<int>(std::log2((xHigh - xLow)/ 1.e-6)) + 1;
+    //std::cout << "max iter = " << maxIter << std::endl;
     for (iter = 0; iter < maxIter; ++iter)
         {
-        std::cout << oldP << std::endl;
-        
-        double slope = (newN - oldN) / (newP - oldP);
-        double nextP = newP - newN / slope;
-        //newP = oldP / mRefIsotherm.loading(mRefTemperature, oldP) * loading;
-        if (std::abs(1.0 - nextP / newP) < 1.e-4)
-            break;
+        xCenter = 0.5 * (xLow + xHigh);
+        fCenter = iso.loading(T, xCenter) - loading;
 
-        oldP = newP;
-        oldN = newN;
-        newP = nextP;
-        newN = mRefIsotherm.loading(mRefTemperature, nextP);
+        if (fCenter > 0.0)
+            {
+            xHigh = xCenter;
+            fHigh = fCenter;
+            }
+        else if (fCenter < 0.0)
+            {
+            xLow = xCenter;
+            fLow = fCenter;
+            }
         }
 
-    if (iter == maxIter)
-        {
-        loadingGuess = mRefIsotherm.loading(mRefTemperature, newP);
-        if (std::abs(1.0 - loadingGuess / loading) > 0.1)
-            throw std::runtime_error
-                {"ItemIsotherm::inverseIsotherm: inversing fails."};
-        }
+    return xCenter;
+    }
 
-    return newP;
+// It would be nice if we implement Invertible decolator.
+//double
+//ItemIsotherm::inverseIsotherm(double loading) const
+//    {
+//    // Ridders' Method.
+//    double P1 = 0.0;
+//    double P2 = 0.0;
+//    double P3 = 0.0;
+//    double P4 = 0.0;
+//    double N1 = 0.0;
+//    double N2 = 0.0;
+//    double N3 = 0.0;
+//    double N4 = 0.0;
+//    double bestP = 0.0;
+//    int iter = 0;
+//    int maxIter = 100;
+//
+//    auto& iso = mRefIsotherm;
+//    auto& T   = mRefTemperature;
+//
+//    bestP = -1.0e30;
+//
+//    P1 = 0.0;
+//    N1 = -loading;
+//
+//    // Find proper P2.
+//    P2 = 1.0;
+//    N2 = iso.loading(T, P2) - loading;
+//
+//     while (N2 < 0.0)
+//        {
+//        P2 *= 1.5;
+//        N2  = iso.loading(T, P2) - loading;
+//
+//        if (P2 > 10000.0)
+//            {
+//            throw std::runtime_error
+//                {"ItemIsotherm::inverseIsotherm(): Given uptake beyonds saturation loading."};
+//            }
+//        }
+//
+//    //std::cout << "P2 = " << P2 << std::endl;
+//    //std::cout << "N2 = " << N2 << std::endl;
+//
+//    // P1 >> P2 >> P3 >> P4 ...
+//    for (iter = 0; iter < maxIter; ++iter)
+//        {
+//        P3 = 0.5 * (P1 + P2);
+//        N3 = iso.loading(T, P3) - loading;
+//
+//        double sign = N1 > N2 ? 1.0 : - 1.0;
+//        double factor = N3 * N3 - N1 * N2;
+//        //std::cout << "factor = " << factor << std::endl;
+//        if (factor < 1.e-10)
+//            break;
+//
+//        P4 = P3 + (P3 - P1) * sign * N3 / std::sqrt(factor);
+//        //std::cout << iter << ", " << P4 << std::endl;
+//
+//        if (std::abs(1.0 - bestP / P4) < 1.e-10)
+//            break;
+//
+//        N4 = iso.loading(T, P4) - loading;
+//        if (std::abs(N4) < 1.e-7)
+//            break;
+//
+//        // Update for next iteration.
+//        if (std::copysign(N3, N4) != N3)
+//            {
+//            P1 = P3;
+//            N1 = N3;
+//            P2 = P4;
+//            N2 = N4;
+//            }
+//        else if (std::copysign(N1, N4) != N1)
+//            {
+//            P2 = P4;
+//            N2 = N4;
+//            }
+//        else if (std::copysign(N2, N4) != N2)
+//            {
+//            P1 = P4;
+//            N1 = P4;
+//            }
+//        else
+//            {
+//            throw std::runtime_error
+//                {"ItemIsotherm::inverseIsotherm(): Weird!"};
+//            }
+//
+//        bestP = P4;
+//
+//        if (std::abs(1.0 - P1 / P2) < 1.e-10)
+//            break;
+//
+//        //std::cout << P4 << std::endl;
+//        }
+//
+//    if (iter == maxIter)
+//        {
+//        throw std::runtime_error
+//            {"ItemIsotherm::inverseIsotherm: inversing fails."};
+//        }
+//
+//    return P4;
+//    }
+
+void
+ItemIsotherm::printPressureList() const
+    {
+    for (auto& p : mPressureList)
+        std::cout << p << std::endl;
     }
--- iast/test/item/test.cpp
+++ iast/test/item/test.cpp
@@ -24,14 +24,27 @@ main()
     cout << (n.spreading_pressure(0.0, P + dP) -
              n.spreading_pressure(0.0, P     )) / dP * P << endl;
 
-
     // Test 2 ====================================================
-    cout << "Test 2" << endl;
+    cout << "Test 2-1" << endl;
     interpolation_isotherm n2 {"h2o.dat"};
-    for (double nn = 0.1; nn < 42.0; nn += 0.1)
-        {
+    for (double nn = 5.0; nn < 45.0; nn += 5.0)
         cout << ItemIsotherm {n2, 293.0, "Q.dat"}.inverseIsotherm(nn) << ", " << nn << endl;
-        }
+
+    cout << "Test 2-2" << endl;
+    for (double nn = 0.1; nn < 10.0; nn += 0.1)
+        cout << ItemIsotherm {n, 293.0, "Q.dat"}.inverseIsotherm(nn) << "   " << nn << endl;
+
+    // Test 3 ====================================================
+    ItemIsotherm itemed = ItemIsotherm {n, 293.0, "Q.dat"};
+
+    cout << "Test 3-1" << endl;
+    for (double p = 0.01; p <= 9.0; p += 0.01)
+        cout << p << "    " << itemed.loading(303.0, p) << endl;
+
+    double value = (itemed.spreading_pressure(303.0, 5.0 + 1.e-6) - itemed.spreading_pressure(303.0, 5.0)) / 1.e-6 * 5.0;
+    cout << itemed.loading(303.0, 5.0) << "     " << value << endl;
+
+    itemed.printPressureList();
 
     return 0;
     }
